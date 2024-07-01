95e68ac084b36b6e9669c6fb13ce32776264dd8e
Diego Guaman <diegoguaman05@hotmail.com>
2024-07-01 20:44:02 +0200

    Rgister now complete recipeChat

--- helthyapp-proyectIII/src/services/ChatService.js
+++ helthyapp-proyectIII/src/services/ChatService.js
@@ -1,6 +1,6 @@
 import createHttp from "./BaseService";
 
-const http = createHttp();
+const http = createHttp(true);
 
 export const createChat = (ingredients) => {
   return http.post("/chat", { ingredients });
